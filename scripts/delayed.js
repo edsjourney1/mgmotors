f5df78ca96161d11330f01b69acc4c27d426fdff
Upadhyaya <305846@cognizant.com>
2024-08-30 18:55:05 +0530

    carousel code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -57,7 +57,7 @@ document.querySelectorAll('.main-image li img').forEach(function(image, index) {
  });
 document.querySelectorAll('.tab-section .default-content-wrapper ul li').forEach(function(tab, index) {
     tab.addEventListener('click', function() {
-        var content = tab.textContent;
+        var content = tab.textContent.toLowerCase();
         // Hide all tab panels
         document.querySelectorAll('.tab-content').forEach(function(panel) {
             panel.style.display = 'none';
