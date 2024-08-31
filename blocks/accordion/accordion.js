76ee38ff45623a44601338a434916576b7ca27d1
franklin <158542216+aem-franklin@users.noreply.github.com>
2024-08-31 23:14:34 +0530

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -22,33 +22,3 @@ export default function decorate(block) {
     });
   }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const accordionContainer = document.querySelector(".mg-motors-accordion");
-    if (!accordionContainer) return;
-
-    const accordionItems = accordionContainer.querySelectorAll(".accordion-item");
-
-    // Close all accordion items except the first one
-    accordionItems.forEach((item, index) => {
-        if (index !== 0) {
-            item.removeAttribute("open");
-        }
-    });
-
-    accordionItems.forEach(item => {
-        item.addEventListener("click", function () {
-            // Close all accordion items except the clicked one
-            accordionItems.forEach(i => {
-                if (i !== this) {
-                    i.removeAttribute("open");
-                }
-            });
-
-            if (!this.hasAttribute("open")) {
-                this.setAttribute("open", "");
-            } else {
-                this.removeAttribute("open");
-            }
-        });
-    });
-
