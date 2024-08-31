5cc381cad9a4dcea7ef788581cb7db8a6c1c5298
franklin <158542216+aem-franklin@users.noreply.github.com>
2024-08-31 23:04:10 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -791,18 +791,3 @@ moveClassToHtmlEl('redesign-v2');
 
 /* EXTERNAL APP CLASS CHECK */
 moveClassToHtmlEl('external-app');
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector('.mg-motors-accordion .accordion-item-label').on('click', function() {
-    // Remove 'active' class from all labels and bodies
-    document.querySelector('.mg-motors-accordion .accordion-item-label p').removeClass('active');
-    document.querySelector('.mg-motors-accordion .accordion-item-body').removeClass('active');
-
-    // Add 'active' class to the clicked label and corresponding body
-    document.querySelector(this).find('p').addClass('active');
-    document.querySelector(this).next('.accordion-item-body').addClass('active');
-  });
-
-  // Automatically trigger click on the first tab to display it initially
-  document.querySelector('.mg-motors-accordion .accordion-item-label:first').trigger('click');
-});
